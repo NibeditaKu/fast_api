c32271c745ee0b8f8e4ff11d55a31099f47a3e3a
Nibedita Roy <nibeditaroy314@gmail.com>
2026-07-05 20:03:54 +0530

    Update welcome message in the root endpoint of main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ teas: List[tea]=[]
 
 @app.get("/")
 def read_root():
-    return {"message": "hello i an nibedita.I love you."}
+    return {"message": "hello i an nibedita.I love you.Dear, i love you."}
 
 @app.get("/teas")
 def get_teas():
