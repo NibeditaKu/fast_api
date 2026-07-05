855720a3ebdc9239d6175fb2d7dd8926e3ad94da
Nibedita Roy <nibeditaroy314@gmail.com>
2026-07-05 19:53:45 +0530

    Fix welcome message formatting in the root endpoint of main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ teas: List[tea]=[]
 
 @app.get("/")
 def read_root():
-    return {"message": "welcome to the tea API. Nibedita Roy,I am from Malda."}
+    return {"message": "welcome to the tea API.I am from Malda."}
 
 @app.get("/teas")
 def get_teas():
